921c15e9c3571ef77c048e865d58ed4c788efc9a
Ecoit-Hugo DESSAUW <dessauw.hugo@gmail.com>
2022-12-01 11:48:44 +0100

    Mise en place Datepicker

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,9 @@ const $ = require('jquery');
 require('bootstrap');
 
 
+$(document).ready(function() {
+// you may need to change this code if you are not using Bootstrap Datepicker
+$('.js-datepicker').datepicker({
+    format: 'yyyy-mm-dd'
+});
+});
