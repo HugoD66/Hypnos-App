04c03b9c04f53b9023e63d033c27beff97a688ab
Ecoit-Hugo DESSAUW <dessauw.hugo@gmail.com>
2022-12-01 13:59:23 +0100

    modif datepicker

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,15 +1,10 @@
-import './styles/app.scss';
-import './bootstrap';
-
-const $ = require('jquery');
-require('bootstrap');
-
 require('bootstrap-datepicker/js/bootstrap-datepicker')
 require('bootstrap-datepicker/js/locales/bootstrap-datepicker.fr')
 require('bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')
-
-
-
-
-
-
+$(document).ready(function (){
+    $('.input-daterange input').each(function () {
+        $(this).datepicker({
+            format: 'dd/mm/YYYY'
+        });
+    });
+});
